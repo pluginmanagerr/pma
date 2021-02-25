3b6f8682fdbc357b5871a4086c28d3da758bd1ed
Kreato <gamzeli_kerim@outlook.com>
2021-02-25 02:11:52 +0000

    added doas support to autoinstall

--- pma.sh
+++ pma.sh
@@ -18,27 +18,39 @@ THEMERESULT=`curl -s https://pluginmanagerr.github.io/pma-repo/vizality.json | j
 if ! [ -x "$(command -v jq)" ]; then
     echo "jq is not installed, attempting to install it"
     sudo pacman -S jq
+    doas pacman -S jq
     sudo apt install jq
+    doas apt install jq
     sudo emerge -a jq --autounmask
+    doas emerge -a jq --autounmask
     sudo zypper install jq
+    doas zypper install jq
     echo "If it didnt work, install  jq manually"
 fi
 
 if ! [ -x "$(command -v curl)" ]; then
     echo "cURL is not installed, attempting to install it"
     sudo pacman -S curl
+    doas pacman -S curl
     sudo apt install curl 
+    doas apt install curl 
     sudo zypper install curl
-    sudo emerge -a curl --autounmask
+    doas zypper install curl
+    sudo emerge -a curl --autounmask  
+    doas emerge -a curl --autounmask
     echo "If it didnt work, install  curl manually"
 fi
 
 if ! [ -x "$(command -v git)" ]; then
     echo "git is not installed, attempting to install it"
     sudo pacman -S git
+    doas pacman -S git
     sudo apt install git 
+    doas apt install git 
     sudo zypper install git
+    doas zypper install git
     sudo emerge -a git --autounmask
+    doas emerge -a git --autounmask
     echo "If it didnt work, install  git manually"
 fi
 
